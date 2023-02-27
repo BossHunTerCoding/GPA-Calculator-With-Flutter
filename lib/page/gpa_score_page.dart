4ad594482ce74130a3ca2f4467669bdea06763dc
BossHunTerCoding <bhtoobossoo@gmail.com>
2023-02-28 02:51:26 +0700

    can edit gpa & navigator score page

--- lib/page/gpa_score_page.dart
+++ lib/page/gpa_score_page.dart
@@ -1,13 +1,60 @@
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:gpa_calculator/main.dart';
 
-class GPAscorePage extends StatelessWidget {
-  const GPAscorePage({super.key});
+class GPAscorePage extends StatefulWidget {
+  const GPAscorePage({super.key,});
+
+  @override
+  State<GPAscorePage> createState() => _GPAscorePageState();
+}
+
+class _GPAscorePageState extends State<GPAscorePage> {
+  AppBar setAppbar() {
+    return AppBar(
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 30),
+        child: IconButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back_ios_new)),
+      ),
+      toolbarHeight: GPAapp.defaultHeightAppbar,
+      backgroundColor: GPAapp.defaultColorsPages,
+      centerTitle: true,
+      title: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: FittedBox(
+          fit: BoxFit.fitWidth,
+          child:
+              GPAapp.setText('GPA Summary'),
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 40),
+          child: IconButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {
+              },
+              icon: const Icon(
+                Icons.restore_from_trash,
+                color: Colors.red,
+                size: 35,
+              )),
+        )
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: setAppbar(),
+    );
   }
-}*/
+}
 
 class GPAscoreDATA {
   GPAscoreDATA(String courseName, String courseGrade, String courseUnit) {
